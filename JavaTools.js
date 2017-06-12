5b0e90b35c7f4bdd2f0aed414c75d58206cf5b55
mwear3robloxandroid <mitchellwears@gmail.com>
2017-06-12 16:45:53 -0400

    Update JavaTools.js

--- JavaTools.js
+++ JavaTools.js
@@ -1,2 +1,4 @@
 function* gen() { 
 2    yield 1;3    yield 2;4    yield 3;5  }6  7  var g = gen(); // "Generator { }"
+function* idMaker() {
+ 2      var index = 0; 3      while(true) 4          yield index++; 5  } 6   7  var gen = idMaker(); // "Generator { }" 8   9  console.log(gen.next().value); // 010  console.log(gen.next().value); // 111  console.log(gen.next().value); // 212  // ...
