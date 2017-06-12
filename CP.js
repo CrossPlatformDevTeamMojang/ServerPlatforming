b8db0a8f0387dada49637fcadf63835212457a92
mwear3robloxandroid <mitchellwears@gmail.com>
2017-06-12 17:15:11 -0400

    Create CP.js

---
+++ CP.js
@@ -0,0 +1,12 @@
+ function="char" '(":Platform"); /("Windows")
+ function="char" '(":Platform"); /("Android")
+function="char" '(":Platform"); /("Xbox One")
+ function="char" '(":Platform"); /("Nintendo Switch")
+  {
+   {
+  function /("packagename"); ("exe"); ("Minecraft");
+  function /("packagename"); ("apk); ("com.mojang.minecraftpe");
+function /("packagename"); ("xex"); ("Minecraft Xbox One Edition")
+function /("packagename")  ("ns"); ("Nintendo Switch")
+{
+ {
